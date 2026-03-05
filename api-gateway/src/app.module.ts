fd599367c74d0ac798f892bd981a841db83fc096
Nikoloz Kvaratskehlia <nikakvaratskhelia11@gmail.com>
2026-03-05 12:50:50 +0400

    setup env conf, connect to db

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserEntity } from './users/entities/user.entity';
+import { RoleEntity } from './users/entities/role.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [
+        UserEntity,
+        RoleEntity
+      ],
+      synchronize: false,
+    }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
