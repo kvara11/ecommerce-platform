a8cb80466312b16bedac985369392c62cb32e3e6
Nikoloz Kvaratskehlia <nikakvaratskhelia11@gmail.com>
2026-03-05 12:20:24 +0400

    es config

--- api-gateway/eslint.config.mjs
+++ api-gateway/eslint.config.mjs
@@ -29,7 +29,13 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      "prettier/prettier": "off",
+      "object-curly-newline": "off",
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unsafe-call': 'warn',
     },
   },
 );
