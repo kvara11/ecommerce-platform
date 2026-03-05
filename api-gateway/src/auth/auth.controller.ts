056980b030f2d7e1afe03882f59b1ec8ba580b89
Nikoloz Kvaratskehlia <nikakvaratskhelia11@gmail.com>
2026-03-05 16:18:02 +0400

    auth contr. import fix

--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RefreshDto } from './dto/refresh.dto';
